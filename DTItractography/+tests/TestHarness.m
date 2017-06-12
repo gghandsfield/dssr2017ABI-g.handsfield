75b5f634539b2b3a5ad2e318bfcbfb03a1d1776d
Geoff Handsfield <g.handsfield@gmail.com>
2017-06-12 12:17:44 +1200

    begin unit testing: YiCao_knnsearch.m

---
+++ DTItractography/+tests/TestHarness.m
@@ -0,0 +1,39 @@
+classdef TestHarness < matlab.unittest.TestCase
+    
+    methods(Test)
+        function testYiCao_knnsearch_Basic(testCase)
+            actSolution = YiCao_knnsearch(1,1,1);
+            expSolution = 1;
+            verifyEqual(testCase,actSolution,expSolution);
+        end
+        
+        function testYiCao_knnsearch_KisTwoError(testCase)
+            verifyError(testCase,@()YiCao_knnsearch(1,1,2),'MATLAB:badsubscript');
+        end
+        
+        function testYiCao_knnsearch_KisTwo(testCase)
+            actSolution = YiCao_knnsearch([1;1],[1;1],2);
+            expSolution = [2 1;1 2];
+            verifyEqual(testCase,actSolution,expSolution);
+        end
+        
+        function testYiCao_knnsearch_multiOut(testCase)
+            [actSolution1,actSolution2] = YiCao_knnsearch([1;1],[1;1],1);
+            expSolution1 = [2;1];
+            expSolution2 = [0;0];
+            verifyEqual(testCase,actSolution1,expSolution1);
+            verifyEqual(testCase,actSolution2,expSolution2);
+        end
+        
+        function testYiCao_knnsearch_multiOut_Coords_BigRef(testCase)
+            [actSolution1,actSolution2] = YiCao_knnsearch([1 1 1;2 2 2;3 3 3;4 4 4],[1 1 1; 3 3 3],1);
+            expSolution1 = [1;3];
+            expSolution2 = [0;0];
+            verifyEqual(testCase,actSolution1,expSolution1);
+            verifyEqual(testCase,actSolution2,expSolution2);
+        end
+                
+    end
+    
+end
+
