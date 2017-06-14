1768c4a12ab4ea3a78f71507b374e1319a05a023
Geoff Handsfield <g.handsfield@gmail.com>
2017-06-14 12:27:03 +1200

    code cleaning: rewrite instructions and redefine vector unitization

--- DTItractography/compPennation.m
+++ DTItractography/compPennation.m
@@ -1,13 +1,17 @@
 function [ deepPennAngle, supPennAngle ] = compPennation( STL, origins, insertions, mean_vector )
-%DTIpennation Compute pennation angle for DTI muscle data. Can also be used
-%for CFD muscle fibers.
-%   STL is the STL input data, origins is the x,y,z locations of origin
-%   tracts. insertions is the x,y,z locations of insertion tracts. origin
-%   and insertion should be the same length. mean_vector is the average
-%   unit vector of the tract, should be the same length as origins.
+%compPennation Compute pennation angles for muscle data. 
+%   INPUTS:
+%   STL is the STL muscle input data
+%   origins is x,y,z locations of n origin tracts (nx3)
+%   insertions is x,y,z locations of n insertion tracts (nx3)
+%   mean_vector is average unit vector of n tracts (nx3)
+%
+%   OUTPUTS:
+%   deepPennAngle is angles between tract and distal aponeurosis (nx1)
+%   supPennAngle is angle between tract and prox aponeurosis (nx1)
 
-
-% fibvecs = origins - insertions;
+%unitize mean vector
+unitvec = mean_vector./sqrt(sum(mean_vector.^2,2));
 
 % find center of every stl face
  facecntr = zeros(length(STL.faces),3);
@@ -23,8 +27,7 @@ function [ deepPennAngle, supPennAngle ] = compPennation( STL, origins, insertio
     end
     
 % Deep Pennation Angle (insertion)
-%     idx_5mm = rangesearch(facecntr,insertions,5);
-    idx_5mm = gh_rangesearch(facecntr,insertions,5);
+    idx_5mm = gh_rangesearch(facecntr,insertions,5); %indices of facecenter within 5mm of insertions
 
     deepPennAngle = zeros(length(idx_5mm),1);
     for rr = 1:length(idx_5mm)
@@ -32,21 +35,17 @@ function [ deepPennAngle, supPennAngle ] = compPennation( STL, origins, insertio
         % mean of face normals within 5mm of each insertion
         MeanNormal = mean(STL.normals(idx_5mm{rr},:),1);
         MeanNormal = MeanNormal./norm(MeanNormal); %unitize face normal
-        
-        % unit vector of DTI fiber vector
-%         unitvec = fibvecs(rr,:)./norm(fibvecs(rr,:));
-        unitvec = mean_vector(rr,:); % mean vector of entire tract
-               
+           
         % dot product and cosine rule to determine pennation angle
-        dp = dot(unitvec,MeanNormal);
+        dp = dot(unitvec(rr,:),MeanNormal);
         deepPennAngle(rr) = acosd( sign(dp).*dp);
     end
     deepPennAngle = abs(real(deepPennAngle) - 90);
     
 %Superficial Pennation Angle (origin)
-    idx_5mm = gh_rangesearch(facecntr,origins,5);
+    idx_5mm = gh_rangesearch(facecntr,origins,5); %indices of facecenter within 5mm of origins
     
-    % not sure what the code below is doing here. Is it needed?
+    % code below is to deal with origins that have no facecenters within 5mm, reassess at 7mm
 %     if find(cellfun(@isempty,idx_5mm))
 %         temp = find(cellfun(@isempty,idx_5mm));
 %         idx_5mm(temp,1) = gh_rangesearch(facecntr,origins(temp,:),7);
@@ -57,13 +56,9 @@ function [ deepPennAngle, supPennAngle ] = compPennation( STL, origins, insertio
         % mean of face normals within 5mm of each insertion
         MeanNormal = mean(STL.normals(idx_5mm{rr},:),1);
         MeanNormal = MeanNormal./norm(MeanNormal); %unitize face normal
-        
-        % unit vector of DTI fiber vector
-%         unitvec = fibvecs(rr,:)./norm(fibvecs(rr,:));
-        unitvec = mean_vector(rr,:); % mean vector of entire tract
-        
+              
         % dot product and cosine rule to determine pennation angle
-        dp = dot(unitvec,MeanNormal);
+        dp = dot(unitvec(rr,:),MeanNormal);
         supPennAngle(rr) = acosd( sign(dp).*dp);
     end
     supPennAngle = abs(real(supPennAngle) - 90);
